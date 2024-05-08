e4e4966f85867aaf2576f154e718c99ef50f2573
José Brito <josealbritose@gmail.com>
2024-05-08 11:15:54 -0300

    check

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,16 +1,18 @@
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
 export const connectDB = async () => {
   try {
-    if(!process.env.DATABASE_URL) throw new Error(`Database uri is missing`)
+    if (!process.env.DATABASE_URL) throw new Error(`Database uri is missing`);
     const connection = await mongoose.connect(process.env.DATABASE_URL);
-    const url = `${connection.connection.host}:${connection.connection.port}/${connection.connection.name}`
-    console.log(`Mongo connect in ${url}`)
-  } catch (error:any) {
-    console.log(error.message)
-    process.exit(1)
+    console.log(connection.connection);
+    const url = `${connection.connection.host}:${connection.connection.port}/${connection.connection.name}`;
+    console.log(url)
+    console.log(`Mongo connect in ${url}`);
+  } catch (error: any) {
+    console.log(error.message);
+    process.exit(1);
   }
-}
+};
